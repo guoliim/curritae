916c24ec3b237884434f4bf306857a1fb7e8e242
guoliim <showerdown2012@gmail.com>
2017-05-12 00:15:01 +0800

    server static file by koa-send  correctly

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 
 const Koa = require('koa')
 const send = require('koa-send')
+const serve = require('koa-static')
 const logger = require('koa-logger')
 const app = new Koa()
 
 app.use(logger())
 
-// 单一加载 并不需要 async await
-app.use(ctx => {
-    send(ctx, './index.html')
+// koa-send api incomplete
+app.use(async ctx => {
+   await send(ctx, ctx.path, {root: __dirname, index: 'index.html'})
 });
+
+// app.use(serve('./', {index: 'index.html'}))
 
 app.on('error', err => console.error('server error', err))
 
